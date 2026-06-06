201ff9ecd86fdb14a4fd5a5729047ab9ce799ec0
ragalik_mac <zubets2442@icloud.com>
2026-06-06 15:44:21 +0300

    still working on add func

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import json
 
-def read_tasks():
-	with open("tasks.json", 'r') as file:
-		data = json.load(file)
-	return data["tasks"]
-
 def add_task(user_task):
     with open("tasks.json", 'r') as file:
         data = json.load(file)
         change = data["tasks"].append(user_task)
     with open("tasks.json", 'w') as file:
-        json.dump(data, file, indent=4)
+        json.dump(change, file)
+
+
+def read_tasks():
+	with open("tasks.json", 'r') as file:
+		data = json.load(file)
+	return data["tasks"]
+
 
 def main():
 	while True:

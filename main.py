5919285da5c0f6a66768d114db05eb09119fa09c
ragalik_mac <zubets2442@icloud.com>
2026-06-09 16:34:22 +0300

    split 'add task' func

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import json
 
-def add_task(user_task):
+def load_data():
     with open("tasks.json", 'r') as file:
         data = json.load(file)
-        
+    return data
+
+def add_task(user_task, data):
+ 
     data["tasks"].append(user_task)
     
     with open("tasks.json", 'w') as file:
@@ -17,23 +20,27 @@ def read_tasks():
 
 
 def main():
-	while True:
-		print("\nПунктц управления")
-		print("[1] Добавить задачу")
-		print("[2] Посмотреть задачи")
-		print("[3] Выход")
   
-		user_choice = int(input())
+    data = load_data()
+    
+    while True: 
+        print("\nПунктц управления") 
+        print("[1] Добавить задачу") 
+        print("[2] Посмотреть задачи") 
+        print("[3] Выход") 
+        
+        user_choice = int(input()) 
+        
+        if user_choice == 1: 
+            user_task = str(input("Введите задачу: ")) 
+            add_task(user_task, data) 
+            print("Задача успешно записана!") 
+        elif user_choice == 2: 
+            print(read_tasks()) 
+        elif user_choice == 3: 
+            print("Досвидания!") 
+            break
 
-		if user_choice == 1:
-			user_task = str(input("Введите задачу: "))
-			add_task(user_task)
-			print("Задача успешно записана!")
-		elif user_choice == 2:
-			print(read_tasks())
-		elif user_choice == 3:
-			print("Досвидания!")
-			break
 
 
 if __name__ == '__main__':
